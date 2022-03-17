f43f8b1dd258ed7f6497180986a377e6919e86b3
Samantha Froude <samantha.froude@Samanthas-MacBook-Pro.local>
2022-03-17 18:24:59 +0000

    gems added

--- Gemfile
+++ Gemfile
@@ -8,3 +8,7 @@ gem "sinatra", "~> 2.2"
 
 gem "gem", "~> 0.0.1.alpha"
 gem "rspec_junit_formatter", "~> 0.5.1"
+
+gem "capybara", "~> 3.36"
+
+gem "rspec", "~> 3.11"
---
+++ spec/server_spec.rb
@@ -0,0 +1,10 @@
+require 'capybara/rspec'
+require 'sinatra' 
+Capybara.app = Sinatra::Application
+
+describe 'home page', type: :feature do
+  it 'displays correct text' do
+    visit '/'
+    expect(page).to have_content('Deploy me to Heroku, please!')
+  end
+end
